63c1d364762023805dd92d3775c8ad8cf5b4eb20
Donatron <don.macarthur68@gmail.com>
2018-06-23 13:43:02 +1000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/Rank/Rank';
+import Rank from './components/ImageLinkForm/ImageLinkForm';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+
+        {/*
+        <FaceRecognition />*/}
+      </div>
+    );
+  }
+}
+
+export default App;
